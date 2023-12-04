ba4f0aaa0c2541cc2dd8f7a8d9a477c092e40a6b
Jay <jasen.strong.js@gmail.com>
2023-12-04 14:35:13 -0500

    updated sqrt(x).py

--- Sqrt(x)/Sqrt(x).py
+++ Sqrt(x)/Sqrt(x).py
@@ -1,8 +1,12 @@
 from math import floor, sqrt
 
-class Solution:
-    def mySqrt(self, x: int) -> int:
-        return floor(sqrt(x))
-    
-sol = Solution.mySqrt(16)
-print(sol)
+class Solution():
+    def __init__(self, x: int) -> None:
+        self.num = x
+
+    def mySqrt(self) -> int:
+        return floor(sqrt(self.num))
+
+number1 = Solution(8)
+sqrt1 = number1.mySqrt()
+print(f"The square root of {number1.num} is {sqrt1}.")
