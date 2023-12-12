9a6cf1e9a95f11abfec57ace0d84585e9037be66
Jay <jasen.strong.js@gmail.com>
2023-12-12 14:49:41 -0500

    stopping for now removeElement.py

--- removeElement/removeElement.py
+++ removeElement/removeElement.py
@@ -2,15 +2,20 @@ class Solution:
     def removeElement(self, nums: list[int], val: int) -> int:
             self.nums = nums
             self.val = val
-            
+            k = []
+            for i in nums:
+                 if i != val:
+                    k.append(i)
+            return k
 
 
 
 
 if __name__ == "__main__":
-
-    arr = [0,1,2,2,3,0,4,2]
-    val = 2
-    print(Solution().removeElement(arr, val))
+    nums = [3,2,2,3]
+    val = 3
+    # nums = [0,1,2,2,3,0,4,2]
+    # val = 2
+    print(Solution().removeElement(nums, val))
 
 # py .\removeElement.py
