221e05151f61f1f9ce1792fe1cd86df5243d093a
Jay <jasen.strong.js@gmail.com>
2023-12-13 10:55:46 -0500

    will finish removeElement.py another time

--- removeElement/removeElement.py
+++ removeElement/removeElement.py
@@ -2,20 +2,25 @@ class Solution:
     def removeElement(self, nums: list[int], val: int) -> int:
             self.nums = nums
             self.val = val
+            original_lenth = len(nums)
             k = []
             for i in nums:
-                 if i != val:
-                    k.append(i)
-            return k
+                    if i != val:  
+                        k.append(i)
+            new_length = len(k)
+            k.sort()
+            for i in range((original_lenth) - new_length):
+                  k.append(0)  
+            return new_length, k
 
 
 
 
 if __name__ == "__main__":
-    nums = [3,2,2,3]
-    val = 3
-    # nums = [0,1,2,2,3,0,4,2]
-    # val = 2
+    # nums = [3,2,2,3]
+    # val = 3
+    nums = [0,1,2,2,3,0,4,2]
+    val = 2
     print(Solution().removeElement(nums, val))
 
 # py .\removeElement.py
