160b6d6a891d45f17ef294c8a436ec0c87d8abf0
Jay <jasen.strong.js@gmail.com>
2024-01-09 14:13:37 -0500

    last update for now

--- removeElement/removeElement.py
+++ removeElement/removeElement.py
@@ -8,8 +8,8 @@ class Solution:
                     if i != val:  
                         k.append(i)
             new_length = len(k)
-            for i in range(original_lenth - new_length):
-                  k.append(0)  
+            # for i in range(original_lenth - new_length):
+            #       k.append(0)  
             return new_length, sorted(k)
 
 
